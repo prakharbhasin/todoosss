414af630e4a2a6d5f436d60856342f3b2f7fa9d4
prkhrbhsn <pbhasin400@gmail.com>
2021-05-30 23:12:50 +0530

    app.js patch

--- src/App.js
+++ src/App.js
@@ -7,31 +7,12 @@ import {Button,Typography} from "@material-ui/core"
 
 function App() {
   return (
-<<<<<<< HEAD
     <Router>
       <Switch>
         <Route path="/" exact component={homePage}/>
         
       </Switch>
     </Router>
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> 1a89697ad08c89329ae15f460bd2327051ea89e5
   );
 }
 
